fa2b787ccb29978b579fae4c3cdb935c7c7912cc
Metehan <metehan.bolat.ie@gmail.com>
2022-09-24 14:40:54 +0300

    Initial commit

---
+++ app/src/main/java/com/metehanbolat/uistructurecompose/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.metehanbolat.uistructurecompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.metehanbolat.uistructurecompose.ui.theme.UIStructureComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UIStructureComposeTheme {
+
+            }
+        }
+    }
+}
