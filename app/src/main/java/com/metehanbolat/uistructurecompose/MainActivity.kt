a4454605928f209a9c3dae06db14478b80ed8cf9
Metehan <metehan.bolat.ie@gmail.com>
2022-09-27 22:16:27 +0300

    Login Page

--- app/src/main/java/com/metehanbolat/uistructurecompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/uistructurecompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.metehanbolat.uistructurecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.metehanbolat.uistructurecompose.login.LoginPage
 import com.metehanbolat.uistructurecompose.ui.theme.UIStructureComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             UIStructureComposeTheme {
-
+                LoginPage()
             }
         }
     }
