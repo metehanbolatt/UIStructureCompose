cf78a40754bc238365d644f62aace86683c40393
Metehan <metehan.bolat.ie@gmail.com>
2022-09-30 00:39:00 +0300

    Application was completed.

--- app/src/main/java/com/metehanbolat/uistructurecompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/uistructurecompose/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.metehanbolat.uistructurecompose
 
 import android.os.Bundle
-import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.metehanbolat.uistructurecompose.login.LoginPage
-import com.metehanbolat.uistructurecompose.login.RegisterPage
+import com.metehanbolat.uistructurecompose.navigation.LoginApplication
 import com.metehanbolat.uistructurecompose.ui.theme.UIStructureComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             UIStructureComposeTheme {
-                RegisterPage()
+                LoginApplication()
             }
         }
     }
