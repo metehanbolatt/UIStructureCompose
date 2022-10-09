b255ce69227d0fec25f2414a8144d2cd3cc43762
Metehan <metehan.bolat.ie@gmail.com>
2022-10-09 22:59:10 +0300

    OutlinedTextField was completed.

--- app/src/main/java/com/metehanbolat/uistructurecompose/components/DropdownMenu.kt
+++ app/src/main/java/com/metehanbolat/uistructurecompose/components/DropdownMenu.kt
@@ -1,10 +1,21 @@
 package com.metehanbolat.uistructurecompose.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.toSize
 
 @Composable
 fun DropdownMenu() {
@@ -14,5 +25,23 @@ fun DropdownMenu() {
 
     var textFieldSize by remember { mutableStateOf(Size.Zero) }
     val icon = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown
+    
+    Column(modifier = Modifier.padding(20.dp)) {
+        OutlinedTextField(
+            value = selectedItem,
+            onValueChange = { selectedItem = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates -> textFieldSize = coordinates.size.toSize() },
+            label = { Text(text = "Select Item") },
+            trailingIcon = {
+                Icon(
+                    imageVector = icon,
+                    contentDescription = null,
+                    Modifier.clickable { expanded = !expanded }
+                )
+            }
+        )
+    }
 
 }
