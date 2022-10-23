91f6876b14afbf9a86835cddce2db685e1fa8035
Metehan <metehan.bolat.ie@gmail.com>
2022-10-23 04:03:23 +0300

    Custom Bar Graph was completed.

--- app/src/main/java/com/metehanbolat/uistructurecompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/uistructurecompose/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.ui.ExperimentalComposeUiApi
-import com.metehanbolat.uistructurecompose.components.OtpView
+import com.metehanbolat.uistructurecompose.components.CustomBarChart
 import com.metehanbolat.uistructurecompose.ui.theme.UIStructureComposeTheme
 
 @ExperimentalComposeUiApi
@@ -14,7 +14,14 @@ class MainActivity : ComponentActivity() {
         actionBar?.hide()
         setContent {
             UIStructureComposeTheme {
-                OtpView()
+                val data = mapOf(
+                    Pair(0.3f, 11),
+                    Pair(0.6f, 13),
+                    Pair(0.2f, 16),
+                    Pair(0.7f, 18),
+                    Pair(0.4f, 19),
+                )
+                CustomBarChart(data = data, maxValue = 1000)
             }
         }
     }
