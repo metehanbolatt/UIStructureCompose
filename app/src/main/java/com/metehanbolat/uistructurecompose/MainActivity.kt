df0d60038a9deb9e4f6a38e8c78b415f4ce642b0
Metehan <metehan.bolat.ie@gmail.com>
2022-11-23 10:31:27 +0300

    Till on touch

--- app/src/main/java/com/metehanbolat/uistructurecompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/uistructurecompose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.metehanbolat.uistructurecompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Button
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.metehanbolat.uistructurecompose.components.tiltOnTouch
 import com.metehanbolat.uistructurecompose.ui.theme.UIStructureComposeTheme
 
 @ExperimentalComposeUiApi
@@ -19,7 +26,23 @@ class MainActivity : ComponentActivity() {
         actionBar?.hide()
         setContent {
             UIStructureComposeTheme {
-                MyComposable()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Image(
+                            modifier = Modifier
+                                .size(width = 400.dp, height = 300.dp)
+                                .tiltOnTouch(),
+                            painter = painterResource(id = R.drawable.sekerbank_kredi_kart),
+                            contentDescription = null
+                        )
+                    }
+                }
             }
         }
     }
