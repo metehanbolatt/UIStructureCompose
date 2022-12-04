bf2c2e5ee8a6a2e1555b35867dad46bb13bdbeef
Metehan <metehan.bolat.ie@gmail.com>
2022-12-04 22:14:55 +0300

    Bottom Sheet was completed with fraction.

--- app/src/main/java/com/metehanbolat/uistructurecompose/components/BottomSheetSample.kt
+++ app/src/main/java/com/metehanbolat/uistructurecompose/components/BottomSheetSample.kt
@@ -1,8 +1,70 @@
 package com.metehanbolat.uistructurecompose.components
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
+@ExperimentalMaterialApi
 @Composable
 fun BottomSheetSample() {
 
+    val sheetState = rememberBottomSheetState(initialValue = BottomSheetValue.Collapsed)
+    val scaffoldState = rememberBottomSheetScaffoldState(bottomSheetState = sheetState)
+    val scope = rememberCoroutineScope()
+
+    BottomSheetScaffold(
+        scaffoldState = scaffoldState,
+        sheetContent = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(300.dp),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "Bottom Sheet", fontSize = 60.sp)
+            }
+        },
+        sheetBackgroundColor = Color.Green.copy(alpha = sheetState.progress.fraction),
+        /** for fully collapsed bottom sheet */
+        //sheetPeekHeight = 0.dp
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Button(
+                onClick = {
+                    scope.launch {
+                        if (sheetState.isCollapsed) {
+                            sheetState.expand()
+                        } else {
+                            sheetState.collapse()
+                        }
+                    }
+                }
+            ) {
+                Text(text = "Bottom Sheet Fraction: ${sheetState.progress.fraction}")
+            }
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Preview
+@Composable
+fun BottomSheetSamplePreview() {
+    BottomSheetSample()
 }
