8ad93ab530f349498d55f3c64a098ee3ab72171a
Metehan <metehan.bolat.ie@gmail.com>
2022-12-10 01:24:47 +0300

    Image Card was completed.

--- app/src/main/java/com/metehanbolat/uistructurecompose/components/ImageCard.kt
+++ app/src/main/java/com/metehanbolat/uistructurecompose/components/ImageCard.kt
@@ -1,8 +1,89 @@
 package com.metehanbolat.uistructurecompose.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.metehanbolat.uistructurecompose.R
 
 @Composable
-fun ImageCard() {
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(
+                    text = title,
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 16.sp
+                    )
+                )
+            }
+        }
+    }
+}
 
+@Preview
+@Composable
+fun ImageCardPreview() {
+    val painter = painterResource(id = R.drawable.snow_animal)
+    val contentDescription = "Lion King"
+    val title = "Lion King in Dark"
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .padding(16.dp)
+    ) {
+        ImageCard(
+            painter = painter,
+            contentDescription = contentDescription,
+            title = title
+        )
+    }
 }
