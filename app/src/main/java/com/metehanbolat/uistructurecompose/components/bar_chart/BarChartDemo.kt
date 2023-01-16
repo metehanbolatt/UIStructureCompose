57094dc5e03b2ea02207c01041d990e99b352650
Metehan <metehan.bolat.ie@gmail.com>
2023-01-16 22:23:49 +0300

    CustomBarChart was completed.

---
+++ app/src/main/java/com/metehanbolat/uistructurecompose/components/bar_chart/BarChartDemo.kt
@@ -0,0 +1,63 @@
+package com.metehanbolat.uistructurecompose.components.bar_chart
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BarChartDemo() {
+
+    val values = mutableListOf(50.dp, 200.dp, 160.dp, 300.dp, 120.dp, 200.dp, 140.dp)
+    val labels = listOf("Mon", "Sun", "Thue", "Wed", "Thu", "Fri", "Sat")
+
+    Column(
+        modifier = Modifier.background(Color.LightGray)
+    ) {
+        Row(
+            modifier = Modifier.height(300.dp),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            values.forEach { value ->
+                CustomBar(
+                    size = value,
+                    max = values.maxOrNull()!!.value,
+                    modifier = Modifier.weight(1f)
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(20.dp),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            labels.forEach { label ->
+                Box(
+                    modifier = Modifier
+                        .weight(1f)
+                        .width(10.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = label
+                    )
+                }
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomBarChartPreview() {
+    BarChartDemo()
+}
